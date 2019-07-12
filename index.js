f6df21615b41678528aea3795b1e2736fb42d852
sehyun hwang <sehyunny6@gmail.com>
2019-07-12 15:19:00 -0400

    add statistics page

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ var app = express();
 // app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-// app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
   res.render('main', { title: 'The index page!' })
+});
+
+app.get('/main', (req, res) => {
+  res.render('main', { title: 'The index page!' })
+});
+
+app.get('/index', (req, res) => {
+  res.render('index', { title: 'The index page!' })
+});
+
+app.get('/statistics', (req, res) => {
+  res.render('statistics', { title: 'The index page!' })
 });
 
 app.listen(3000, function () {
