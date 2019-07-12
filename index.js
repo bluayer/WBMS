a6cdd0e3906164754d087b2afeaa89eb86b717b5
sehyun hwang <sehyunny6@gmail.com>
2019-07-12 14:26:25 -0400

    initial web

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 
+// app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+// app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'The index page!' })
+  res.render('main', { title: 'The index page!' })
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!!');
 });
