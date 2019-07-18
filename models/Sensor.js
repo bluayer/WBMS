a4380faa2f2598235c2abe5aae0573a370525621
hktomato <khk04078@gmail.com>
2019-07-19 04:07:37 +0900

    1

--- models/Sensor.js
+++ models/Sensor.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const sensorSchema = new Schema({
+  pi_id: Number,
   temperature: Number,
   soc: Number,
+  location: String,
+  t_min: Number,
+  t_max: Number,
   date: { type: Date, default: Date.now },
 });
 
