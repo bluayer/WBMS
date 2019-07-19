b5b5d2db9a712d2436a6ab648e1e98097c053902
Daniel Song <ijacsong98@gmail.com>
2019-07-19 10:55:02 -0400

    Add management code for battery
    When temperature up and down, fan or heater on/off.
    DB schema is little bit changed for using some data from raspberry pi.

---
+++ routes/pisensor.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const PiSensor = require('../models/PiSensor');
+const management = require('./management');
+
+const Console = console;
+const router = express.Router();
+
+// GET '/sensor'
+// Just render test.ejs
+router.get('/', (req, res) => {
+  PiSensor.find().sort({ date: 1 }).exec((err, sensors) => {
+    if (err) {
+      Console.log(err);
+      res.json(err);
+    }
+    res.render('sensor', { sensors });
+  });
+});
+
+// POST '/sensor'
+// Save data at DB
+router.post('/', (req, res) => {
+  const tempMin = 0;
+  const tempMax = 40;
+
+  const {
+    id, temperature, batteryRemain, location, date,
+  } = req.body;
+
+  const piSensor = new PiSensor();
+
+  piSensor.id = id;
+  piSensor.temperature = temperature;
+  piSensor.batteryRemain = batteryRemain;
+  piSensor.location = location;
+  piSensor.tempMin = tempMin;
+  piSensor.tempMax = tempMax;
+  piSensor.date = new Date(date);
+
+  piSensor.save((err) => {
+    if (err) {
+      Console.error(err);
+    }
+    Console.log('Save okay');
+  });
+
+  res.json(management.makeMessage(temperature, tempMin, tempMax, batteryRemain));
+});
+
+module.exports = router;
