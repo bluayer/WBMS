b7100751183a9894339ca5adc0a810970940ad9d
hktomato <khk04078@gmail.com>
2019-08-08 11:02:11 +0900

    fix : kpload function, disconnected function

--- public/javascript/disconnectedSituation.js
+++ public/javascript/disconnectedSituation.js
@@ -11,7 +11,6 @@ const disconnectedSituation = async (id, latitude, longitude) => {
   for (let i = 0; i < 5; i += 1) {
     const packageTemp = dayPredictArgo.dayPredictArgo(id, latitude, longitude, i + 1);
     PiInfo.find({ id }).exec((err, packageData) => {
-      Console.log('asdfkasdojf;ldkasjf;klasdjfklsdalkf시발 : ', packageData);
       for (let j = 0; j < 8; j += 1) {
         const data = management.manageTemperature(packageTemp[j], packageData.tempMin, packageData.tempMax);
         jsonContent.push(data);
