d2ffe36e8811a6d7e7c4ee3d82e33206f4ff020a
Daniel Song <ijacsong98@gmail.com>
2019-08-08 18:01:30 -0400

    Hotfix : Disconnected Situation complete
    There's errors when we merge HotLoc branch(almost code about dayPredict and it changed a lot).
    I fixed it for working.

--- public/javascript/disconnectedSituation.js
+++ public/javascript/disconnectedSituation.js
@@ -4,24 +4,51 @@ const PiInfo = require('../../models/PiInfo');
 
 const Console = console;
 
+const getOneDayAction = (id, latitude, longitude, k) => {
+  const oneDayAction = dayPredictArgo.dayPredictArgo(id, latitude, longitude, k)
+    .then((packageTemp) => {
+      const actionArray = PiInfo.find({ id }).exec().then((packageData) => {
+        const res = [];
+        for (let j = 0; j < 8; j += 1) {
+          const temp = packageTemp[0][j].split('/');
+          const temperature = Number(temp[0]);
+          const date = temp[1];
+          const data = management.manageTemperature(
+            temperature, packageData[0].tempMin, packageData[0].tempMax,
+          );
+          data.delay = date;
+          res.push(data);
+        }
+        // 하루치 액션이 담긴 배열
+        return res;
+      }).then(response => response);
 
-const disconnectedSituation = (id, latitude, longitude) => {
-  const jsonContent = [];
+      return actionArray;
+    }).then(arr => arr);
+
+  return oneDayAction;
+};
+
+const getFiveDayAction = (id, latitude, longitude) => {
+  const content = [];
   for (let i = 0; i < 5; i += 1) {
-    const packageTemp = dayPredictArgo.dayPredictArgo(id, latitude, longitude, i + 1);
-    PiInfo.find({ id }).exec((err, packageData) => {
-      Console.log('packageData.tempMax: ', packageData[0].tempMax);
-      for (let j = 0; j < 8; j += 1) {
-        const data = management.manageTemperature(
-          packageTemp[j], packageData[0].tempMin, packageData[0].tempMax,
-        );
-        jsonContent.push(data);
-      }
-    });
+    content.push(getOneDayAction(id, latitude, longitude, i + 1)
+      .then(actions => actions));
   }
-  Console.log(jsonContent);
 
-  return jsonContent;
+  return Promise.all(content);
+};
+const disconnectedSituation = async (id, latitude, longitude) => {
+  return getFiveDayAction(id, latitude, longitude).then((content) => {
+    const data = [];
+    content.forEach((oneDayAction) => {
+      oneDayAction.forEach((action) => {
+        data.push(action);
+      });
+    });
+
+    return Promise.all(data);
+  });
 };
 
 module.exports = { disconnectedSituation };
